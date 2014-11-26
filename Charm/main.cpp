989a6538a1d0216c2f546eee9350ce12f74044ae
Onur Yüksek <onur.yueksek@kdab.com>
2014-11-26 17:32:37 +0100

    Connect to Charm database on startup

--- Charm/main.cpp
+++ Charm/main.cpp
@@ -1,13 +1,83 @@
-//#include <QApplication>
-//#include <QQmlApplicationEngine>
+#include <QGuiApplication>
+#include <QQmlApplicationEngine>
+#include <QSettings>
 
-#include <QtGui/QGuiApplication>
-#include <QtQml/QQmlApplicationEngine>
+#include "CharmConstants.h"
+#include "CharmDataModel.h"
+#include "Controller.h"
+#include "EventModelAdapter.h"
+
+class Application : public QGuiApplication
+{
+    Q_OBJECT
+
+public:
+    Application(int &argc, char **argv)
+        : QGuiApplication(argc, argv)
+    {
+        // note that this modifies the behaviour of QSettings:
+        setOrganizationName("KDAB");
+        setOrganizationDomain("kdab.com");
+        setApplicationName("Charm");
+        setApplicationVersion(CHARM_VERSION);
+
+        connectControllerAndModel(&m_controller, &m_dataModel);
+    }
+
+    int run()
+    {
+        if (not readConfiguration()) {
+            qWarning("Could not read configuration");
+            return EXIT_FAILURE;
+        }
+
+        if (not m_controller.initializeBackEnd(CHARM_SQLITE_BACKEND_DESCRIPTOR)) {
+            qWarning("Could not initialize to backend");
+            return EXIT_FAILURE;
+        }
+
+        if (not m_controller.connectToBackend()) {
+            qWarning("Could not connect to backend");
+            return EXIT_FAILURE;
+        }
+
+        EventModelAdapter events(&m_dataModel);
+        QQmlApplicationEngine engine(QUrl("qrc:///qml/MainScreen.qml"));
+
+        return exec();
+    }
+
+private:
+    bool readConfiguration()
+    {
+        // TODO: detect empty config file and generate something like this if needed:
+        //
+        // [debug]
+        // InstallationId=1
+        // LocalStorageDatabase=/home/onur/.local/share/KDAB/Charm/Charm-Android.db
+        // LocalStorageType=sqlite
+        // UserId=1
+        //
+        // see Application::configure() -> if (!configurationComplete || CONFIGURATION.failure)
+        //
+        QSettings settings;
+
+        qDebug("Reading configuration from group %s of %s",
+               qPrintable(settings.fileName()),
+               qPrintable(CONFIGURATION.configurationName));
+
+        settings.beginGroup(CONFIGURATION.configurationName);
+        return CONFIGURATION.readFrom(settings);
+    }
+
+private:
+    CharmDataModel m_dataModel;
+    Controller m_controller;
+};
 
 int main(int argc, char *argv[])
 {
-    QGuiApplication app(argc, argv);
-    QQmlApplicationEngine engine(QUrl("qrc:///qml/MainScreen.qml"));
-
-    return app.exec();
+    return Application(argc, argv).run();
 }
+
+#include "main.moc"
